39d6b3798a9cc9be6381ad1feffea12b7d78ee6e
Keroro <13545141090@163.com>
2017-04-05 00:09:53 +0800

    2017-04-05 00:09

---
+++ gprs/src/main/java/com/shlr/gprs/dao/ChannelResourceMapper.java
@@ -0,0 +1,19 @@
+package com.shlr.gprs.dao;
+
+import java.util.List;
+
+import org.mybatis.spring.annotation.MapperScan;
+
+import com.shlr.gprs.domain.ChannelResource;
+
+import tk.mybatis.mapper.common.Mapper;
+
+/**
+* @author xucong
+* @version 创建时间：2017年4月4日 下午11:30:59
+* 
+*/
+@MapperScan
+public interface ChannelResourceMapper extends Mapper<ChannelResource>{
+	List<ChannelResource> querylist();
+}
